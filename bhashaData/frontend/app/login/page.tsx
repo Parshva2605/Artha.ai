e6bed0d0a27d8f6087661be1c7c8a83b413277f5
Parshva <shahparshva2005@gmail.com>
2026-04-08 20:40:07 +0530

    bug fix

--- bhashaData/frontend/app/login/page.tsx
+++ bhashaData/frontend/app/login/page.tsx
@@ -89,7 +89,7 @@ export default function LoginPage() {
 
         {/* Signup Link */}
         <p className="text-center text-gray-600 mt-6">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-orange-600 hover:text-orange-700 font-semibold">
             Create one
           </Link>
